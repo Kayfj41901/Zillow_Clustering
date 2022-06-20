85ce8179b0e60db1f028ae2bb62ac0f634f1252b
Kayla Brock <kayla.marie.brock@gmail.com>
2022-06-20 10:12:46 -0500

    update

--- imports.py
+++ imports.py
@@ -1,5 +1,6 @@
-import os
+
 # ignore warnings
+import os
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -7,30 +8,34 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 import numpy as np
 
+import sklearn.metrics
+
 # Exploring
 import scipy.stats as stats
 
 # Visualizing
-%matplotlib inline
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split 
 from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.cluster import KMeans
 import env
 import visuals
+from scipy.stats import f_oneway
 
 import wrangle_zillow
 import scipy.stats as stats
+
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 
 pd.options.display.max_rows = 100 
 
 # default pandas decimal number display format
 pd.options.display.float_format = '{:20,.2f}'.format
-
-
-
 
 
 
